2228f85b1244a00f2a14360709a0810c57af62a0
bmhato <biplab.mahato@capgemini.com>
2018-09-26 14:40:39 +0530

    Drupal8 Form API

---
+++ modules/custom/d8_routing/src/Form/SimpleForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\d8_routing\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SimpleForm extends FormBase {
+
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\FormInterface::getFormId()
+  */
+ public function getFormId() {
+  // TODO: Auto-generated method stub
+  return 'simple form';
+ }
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\FormInterface::buildForm()
+  */
+ public function buildForm(array $form, FormStateInterface $form_state) {
+  $form ['name'] = array (
+    '#type' => 'textfield',
+    '#title' => t ( 'Full Name:' ),
+    '#required' => TRUE
+  );
+  $form ['mail'] = array (
+    '#type' => 'email',
+    '#title' => t ( 'Email ID:' ),
+    '#required' => TRUE
+  );
+  $form ['actions'] ['submit'] = array (
+    '#type' => 'submit',
+    '#value' => $this->t ( 'Submit' ),
+    '#button_type' => 'primary'
+  );
+
+  return $form;
+ }
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\FormBase::validateForm()
+  */
+ public function validateForm(array &$form, FormStateInterface $form_state) {
+  if (strlen ( $form_state->getValue ( 'name' ) ) < 5) {
+   $form_state->setErrorByName ( 'name', $this->t ( 'Name must not be less than 5 chars.' ) );
+  }
+ }
+
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\FormInterface::submitForm()
+  */
+ public function submitForm(array &$form, FormStateInterface $form_state) {
+  // TODO: Auto-generated method stub
+  $this->messenger ()->addMessage ( $this->t ( 'Form submitted successfully.' ) );
+ }
+}
