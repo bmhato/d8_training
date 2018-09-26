34bf0bbff1a5df8ef7047ba63f69549e419700ef
bmhato <biplab.mahato@capgemini.com>
2018-09-26 15:48:03 +0530

    Weather Config form

---
+++ modules/custom/d8_routing/src/Form/WeatherConfigForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\d8_routing\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class WeatherConfigForm extends ConfigFormBase {
+
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\FormInterface::getFormId()
+  */
+ public function getFormId() {
+  // TODO: Auto-generated method stub
+  return 'd8_routing_demo_config_form';
+ }
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\ConfigFormBase::buildForm()
+  */
+ public function buildForm(array $form, FormStateInterface $form_state) {
+  $form ['app_id'] = array (
+    '#type' => 'textfield',
+    '#title' => t ( 'App ID:' ),
+    '#required' => TRUE
+  );
+  $form ['actions'] ['submit'] = array (
+    '#type' => 'submit',
+    '#value' => $this->t ( 'Submit' ),
+    '#button_type' => 'primary'
+  );
+
+  return $form;
+ }
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\ConfigFormBase::submitForm()
+  */
+ public function submitForm(array $form, FormStateInterface $form_state) {
+ }
+
+ /**
+  *
+  * {@inheritdoc}
+  *
+  * @see \Drupal\Core\Form\ConfigFormBaseTrait::getEditableConfigNames()
+  */
+ protected function getEditableConfigNames() {
+  // TODO: Auto-generated method stub
+ }
+}
